2bfba94c944b6a3744a2c380961950ca24b262a9
Danil <93711640+p1ck234@users.noreply.github.com>
2023-01-17 22:20:55 +0400

    исправил умножение

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@ namespace matrix
 		static void Main(string[] args)
 		{
 
-			int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6, }, { 7, 8, 9 } };
+			int[,] matrix = new int[,] { { 1, 2, 3,4 }, { 4, 5, 6,4 }, { 7, 8, 9,4 } };
+            int[,] secondmatrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6}, { 7, 8, 9} };
 
-			//Transp(matrix);
+            //Transp(matrix);
 
-			//Summ(matrix);
+            //Summ(matrix);
 
-			//Multiply(matrix);
+            Multiply(matrix, secondmatrix);
 
 			//int numb = 2;
 			//MultiplyOnNumb(matrix, numb);
@@ -21,7 +22,9 @@ namespace matrix
 		}
 		public static void Transp(int[,] matrix)
 		{
-			Console.WriteLine("Old Matrix:");
+            Console.WriteLine("Transpose");
+
+            Console.WriteLine("Old Matrix:");
 			for (int i = 0; i < matrix.GetLength(0); i++)
 			{
 				for (int j = 0; j < matrix.GetLength(1); j++)
@@ -54,7 +57,9 @@ namespace matrix
 
 		public static void Summ(int[,] matrix)
 		{
-			int[,] secondMatrix = new int[,] { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+            Console.WriteLine("Summ");
+
+            int[,] secondMatrix = new int[,] { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
 			int[,] temp = new int[matrix.GetLength(0), matrix.GetLength(1)];
 
 
@@ -101,40 +106,46 @@ namespace matrix
 			Console.WriteLine();
 		}
 
-		public static void Multiply(int[,] matrix)
+        public static void Multiply(int[,] matrix, int[,] secondMatrix)
+        {
+
+            int[,] Mass = new int[secondMatrix.GetLength(0), secondMatrix.GetLength(1)];//[1, 7]; // массив для записи результата проецирования
+            int sum = 0;
+
+            Console.WriteLine("Multiply");
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)  // умножение
+            {
+                for (int j = 0; j < secondMatrix.GetLength(1); j++)
+                {
+                    for (int r = 0; r < secondMatrix.GetLength(0); r++)
+                    {
+                        sum = sum + matrix[i, r] * secondMatrix[r, j];
+                    }
+                    Mass[i, j] = sum;
+                    sum = 0;
+                }
+            }
+
+
+            for (int i = 0; i < Mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < Mass.GetLength(1); j++)
+                {
+                    Console.Write(Mass[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+
+        public static void MultiplyOnNumb(int[,] matrix, int number)
 		{
-			int[,] secondMatrix = new int[,] { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
-			int[,] temp = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            Console.WriteLine("Multiplyonnumb");
 
-			for (int i = 0; i < matrix.GetLength(0); i++)
-			{
-				for (int j = 0; j < matrix.GetLength(1); j++)
-				{
-					for (int k = 0; k < matrix.GetLength(0); k++)
-					{
-						temp[i, j] += matrix[i, k] * secondMatrix[k, j];
-					}
-				}
-			}
-
-
-
-			for (int i = 0; i < matrix.GetLength(0); i++)
-			{
-				for (int j = 0; j < matrix.GetLength(1); j++)
-				{
-					Console.Write(temp[i, j] + " ");
-				}
-				Console.WriteLine();
-			}
-			Console.WriteLine();
-		}
-
-
-
-		public static void MultiplyOnNumb(int[,] matrix, int number)
-		{
-			int[,] secondMatrix = new int[,] { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+            int[,] secondMatrix = new int[,] { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
 			int[,] temp = new int[matrix.GetLength(0), matrix.GetLength(1)];
 
 			for (int i = 0; i < matrix.GetLength(0); i++)
@@ -176,7 +187,9 @@ namespace matrix
 
 		public static void ReadFromFile()
 		{
-			string outputpath = "output.txt";
+            Console.WriteLine("ReadFromFile");
+
+            string outputpath = "output.txt";
 
 			string[,] matrix = new string[3, 3];
 
